cf523e978f4aaf263de9ebdd06b0db71159e9633
Iftikhar2425 <iftikharalisyed001@gmail.com>
2026-02-18 00:33:13 +0500

    Prepare Django project for Vercel deployment

--- invoice_proj/settings.py
+++ invoice_proj/settings.py
@@ -20,12 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&acdo!wqt@6h-w1lrsww_qoeco2lwp@utjahjk0!gyd=1$52dp')
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 
+    'django-insecure-&acdo!wqt@6h-w1lrsww_qoeco2lwp@utjahjk0!gyd=1$52dp'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+
+ALLOWED_HOSTS = ['*']  
 
 
 # Application definition
