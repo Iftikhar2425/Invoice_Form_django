c324f02a64a24af9b30e133033f73e03be3ccc0f
Iftikhar2425 <iftikharalisyed001@gmail.com>
2026-02-17 23:08:03 +0500

    Added auto invoice number and custom admin panel

--- invoice_proj/settings.py
+++ invoice_proj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&acdo!wqt@6h-w1lrsww_qoeco2lwp@utjahjk0!gyd=1$52dp'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&acdo!wqt@6h-w1lrsww_qoeco2lwp@utjahjk0!gyd=1$52dp')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 
 
