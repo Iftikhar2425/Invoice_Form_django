99c25ebfb2ce8fada5fadf872bbea2a4685f5a3d
Iftikhar2425 <iftikharalisyed001@gmail.com>
2026-02-17 22:34:39 +0500

    Initial commit - Django project with custom admin

--- invoice/admin.py
+++ invoice/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Invoice, InvoiceItem
 
-admin.site.register(Invoice)
-admin.site.register(InvoiceItem)
+class InvoiceItemInline(admin.TabularInline):
+    model = InvoiceItem
+    extra = 1  # Number of blank items
+
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('invoice_no', 'customer_name', 'date', 'total_amount')
+    search_fields = ('invoice_no', 'customer_name', 'license_no')
+    list_filter = ('date',)
+    readonly_fields = ('invoice_no',)
+    inlines = [InvoiceItemInline]  # Show items inline in invoice
+
+admin.site.register(Invoice, InvoiceAdmin)
